307359f147355a6e9f7a9b3457e3a062a44a5895
Rad-hi <radhisghaier33@gmail.com>
2021-10-24 20:57:25 +0100

    My first Rust program ever

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+/* https://www.rust-lang.org/learn/get-started */
+
+use ferris_says::say;
+use std::io::{stdout, BufWriter};
+
+fn main(){
+    let stdout = stdout();
+    let message = String::from("I'm Ferris, welcome!");
+    let width = message.chars().count();
+
+    let mut writer = BufWriter::new(stdout.lock());
+    say(message.as_bytes(), width, &mut writer).unwrap();
+}
